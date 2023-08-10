7e776ee1715400cb2bd7dfadf0fb8066f7cce7fb
rodyrahi <rodyrahi126@gmail.com>
2023-08-10 02:27:23 -0700

    ok

--- whatsappapi.js
+++ whatsappapi.js
@@ -9,21 +9,28 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/:number/:message', (req, res) => {
-  const number = req.params.number;
-  const formattedNumber = '91' + number.toString();
-  const message = req.params.message.replace(/['\[\]]/g, ''); // Remove single quotes, square brackets from the message
+app.post('/send-message', (req, res) => {
+  try {
+    const { number, message } = req.body;
 
-  console.log(formattedNumber);
+    const formattedNumber = '91' + number.toString();
+    const sanitizedMessage = message.replace(/['\[\]]/g, ''); // Remove single quotes, square brackets from the message
 
-  client
-    .sendMessage(`${formattedNumber}@c.us`, message)
-    .then(() => res.json({ status: 'ok' })) // Send JSON response with "ok"
-    .catch((error) => {
-      console.error('Error:', error);
-      res.status(500).json({ status: 'error' }); // Send JSON response with "error"
-    });
+    console.log(formattedNumber);
+
+    client
+      .sendMessage(`${formattedNumber}@c.us`, sanitizedMessage)
+      .then(() => res.json({ status: 'ok' })) // Send JSON response with "ok"
+      .catch((error) => {
+        console.error('Error:', error);
+        res.status(500).json({ status: 'error' }); // Send JSON response with "error"
+      });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ status: 'error' }); // Send JSON response with "error"
+  }
 });
+
 
 
 app.listen(port, () => {
